16277537ca43f3e30a359dbc787442c9cf118edb
Shuntay Edwards <shuntay_edwards@redlands.edu>
2020-07-30 12:57:05 -0700

    Portfolio

---
+++ assets/files/Recipe Website/js/recipe.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('#avocadotoggle').click(function () {
+        $('#avocadorecipe').toggle();
+    });
+    $('#spinachsaladtoggle').click(function () {
+        $('#spinachsaladrecipe').toggle();
+    });
+    $('#cbtoggle').click(function () {
+        $('#cbrecipe').toggle();
+    });
+    $('#yogurttoggle').click(function () {
+        $('#yogurtrecipe').toggle();
+    });
+    $('#pancaketoggle').click(function () {
+        $('#pancakerecipe').toggle();
+    });
+    $('#squashtoggle').click(function () {
+        $('#squashrecipe').toggle();
+    });
+    $('#pastatoggle').click(function () {
+        $('#pastarecipe').toggle();
+    });
+    $('#burritotoggle').click(function () {
+        $('#burritorecipe').toggle();
+    });
+    $('#tacotoggle').click(function () {
+        $('#tacorecipe').toggle();
+    });
+    $('#ssmoothietoggle').click(function () {
+        $('#ssmoothierecipe').toggle();
+    });
+    $('#gsmoothietoggle').click(function () {
+        $('#gsmoothierecipe').toggle();
+    });
+    $('#bsmoothietoggle').click(function () {
+        $('#bsmoothierecipe').toggle();
+    });
+    });
